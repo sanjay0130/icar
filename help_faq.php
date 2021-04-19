64f91aa47441e32af18ef9bee0c6a5a6c950c3a7
Sanjay Kumar Das <sanjay.das@krazy-tech.com>
2021-04-19 15:48:26 +0530

    FAQ page added

--- help_faq.php
+++ help_faq.php
@@ -53,51 +53,46 @@
            
             <div class="col-sm-12">
               <h3>Freaquently Asked Questions</h3>
-              <div id="accordion">
-  <div class="card">
-    <div class="card-header" id="headingOne">
-      <h5>
-        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-          Collapsible Group Item #1
-        </button>
-      </h5>
-    </div>
 
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingTwo">
-      <h5>
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-          Collapsible Group Item #2
-        </button>
-      </h5>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-  <div class="card">
-    <div class="card-header" id="headingThree">
-      <h5>
-        <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-          Collapsible Group Item #3
-        </button>
-      </h5>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
-      <div class="card-body">
-        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-      </div>
-    </div>
-  </div>
-</div>
+              <?php
+              include('admin/common/database.mysqli.php');
+              $con_obj   = new common();
+              $fetch_res = $con_obj->select('faq', '*', NULL, NULL, NULL);
+              
+              ?>
+
+              <div id="accordion">
+
+                <?php
+                  if($con_obj->numRows() > 0) {
+                    $ctr = 0;
+                    foreach ($con_obj->getResult() as $key => $value) {
+
+                      $header = $value['title'];
+                      $rand   = rand(1,1000000);
+                ?>
+                      <div class="card">
+                        <div class="card-header" id="heading<?php echo $rand; ?>">
+                          <h5>
+                            <button class="btn btn-link" data-toggle="collapse" data-target="#collapse<?php echo $rand; ?>" aria-expanded="true" aria-controls="collapse<?php echo $rand; ?>">
+                              <?php echo $header; ?>
+                            </button>
+                          </h5>
+                        </div>
+
+                        <div id="collapse<?php echo $rand; ?>" class="collapse show" aria-labelledby="heading<?php echo $rand; ?>" data-parent="#accordion">
+                          <div class="card-body">
+                            <?php echo $value['description']; ?>
+                          </div>
+                        </div>
+                      </div>
+
+                <?php 
+                    }
+                }
+                ?>
+ 
+              </div>
             </div>
           </div>
         </div>
